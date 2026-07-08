9c90957a03dec6d5a1708bdab225464c3d2d31d1
maulopez4 <44982011+maulopez4@users.noreply.github.com>
2026-07-08 11:20:27 -0700

    Refactor lamp_stack.sh to remove sudo usage
    Removed 'sudo' from installation commands for non-root execution.

--- lamp_stack.sh
+++ lamp_stack.sh
@@ -2,58 +2,59 @@
 
 # Actualizar el sistema
 echo "Actualizando el sistema..."
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl php-cli php-mbstring git unzip
+apt update && sudo apt upgrade -y
+apt install curl php-cli php-mbstring git unzip
 
 #Instalando Webmin
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
-sudo sh webmin-setup-repo.sh
-sudo apt-get install webmin --install-recommends
+sh webmin-setup-repo.sh
+apt-get install webmin --install-recommends
 
 # Instalar Apache
 echo "Instalando Apache..."
-sudo apt install -y apache2 apache2-utils
-sudo systemctl enable apache2
-sudo systemctl start apache2
+apt install -y apache2 apache2-utils
+systemctl enable apache2
+systemctl start apache2
 
 # Instalar MariaDB
 echo "Instalando MariaDB..."
-sudo apt install -y mariadb-server mariadb-client
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
+apt install -y mariadb-server mariadb-client
+systemctl enable mariadb
+systemctl start mariadb
 
-# Instalar PHP 8.4 y módulos comunes
-echo "Instalando PHP 8.4..."
+# Instalar PHP y módulos comunes
+echo "Instalando PHP..."
 sudo apt install -y php libapache2-mod-php php-mysql php-cli php-curl php-gd php-mbstring php-xml php-zip
 
 # Crear Certificado local
-sudo apt install ssl-cert
-sudo make-ssl-cert generate-default-snakeoil --force-overwrite
-sudo a2enmod ssl
-sudo systemctl restart apache2
+apt install ssl-cert
+make-ssl-cert generate-default-snakeoil --force-overwrite
+a2enmod ssl
+systemctl restart apache2
 
 # Instalar ProFTPd
-sudo apt install proftpd -y
+apt install proftpd -y
 
 # Instalar Composer
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo rm /tmp/composer-setup.php
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm /tmp/composer-setup.php
 
 # Install NPM
 curl -fsSL https://deb.nodesource.com -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs
+bash nodesource_setup.sh
+apt install nodejs
 
 # Instalar UFW
-sudo apt install -y ufw
-sudo ufw allow http
-sudo ufw allow https
-sudo ufw allow 10000
-sudo ufw allow ssh
-sudo ufw allow ftp
-sudo ufw enable
-sudo ufw reload
+apt install -y ufw
+ufw allow http
+ufw allow https
+ufw allow 10000
+ufw allow 3306
+ufw allow ssh
+ufw allow ftp
+ufw enable
+ufw reload
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
@@ -101,6 +102,23 @@ echo "Shell: /bin/bash"
 echo "Password change required on first login."
 
 # Ajustar permisos para /var/www/html
+chmod o+x /home/$USERNAME
+mkdir -p /home/$USERNAME/public_html
+chmod 755 /home/$USERNAME/public_html
+ln -s /var/www/html/$USERNAME /home/$USERNAME/public_html
+touch /etc/apache2/sites-available/$USERNAME.conf
+cat $USERNAME.conf <<EOF
+<VirtualHost *:81>
+    ServerAdmin webmaster@yourdomain.com
+    ServerName $USERNAME
+    ServerAlias : $USERNAME
+    DocumentRoot /var/www/html/$USERNAME
+
+    ErrorLog ${APACHE_LOG_DIR}/$USERNAME_error.log
+    CustomLog ${APACHE_LOG_DIR}/$USERNAME__access.log combined
+</VirtualHost>
+EOF
+systemctl restart apache2
 #sudo chown -R $USERNAME:www-data /var/www/html
 #sudo chmod -R 775 /var/www/html
 
