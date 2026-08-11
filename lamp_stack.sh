4c599e3626dfa9472e1b8415125f88285708eedf
maulopez4 <44982011+maulopez4@users.noreply.github.com>
2026-08-10 22:16:50 -0700

    Add '-y' option to package installations in script

--- lamp_stack.sh
+++ lamp_stack.sh
@@ -3,7 +3,7 @@
 # Actualizar el sistema
 echo "Actualizando el sistema..."
 apt update && sudo apt upgrade -y
-apt install curl php-cli php-mbstring git unzip
+apt install curl php-cli php-mbstring git unzip -y
 
 #Instalando Webmin
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
@@ -12,22 +12,22 @@ apt-get install webmin --install-recommends
 
 # Instalar Apache
 echo "Instalando Apache..."
-apt install -y apache2 apache2-utils
+apt install apache2 apache2-utils -y
 systemctl enable apache2
 systemctl start apache2
 
 # Instalar MariaDB
 echo "Instalando MariaDB..."
-apt install -y mariadb-server mariadb-client
+apt install mariadb-server mariadb-client -y
 systemctl enable mariadb
 systemctl start mariadb
 
 # Instalar PHP y módulos comunes
 echo "Instalando PHP..."
-sudo apt install -y php libapache2-mod-php php-mysql php-cli php-curl php-gd php-mbstring php-xml php-zip
+sudo apt install php libapache2-mod-php php-mysql php-cli php-curl php-gd php-mbstring php-xml php-zip -y
 
 # Crear Certificado local
-apt install ssl-cert
+apt install ssl-cert -y
 make-ssl-cert generate-default-snakeoil --force-overwrite
 a2enmod ssl
 systemctl restart apache2
@@ -43,10 +43,10 @@ rm /tmp/composer-setup.php
 # Install NPM
 curl -fsSL https://deb.nodesource.com -o nodesource_setup.sh
 bash nodesource_setup.sh
-apt install nodejs
+apt install nodejs -y
 
 # Instalar UFW
-apt install -y ufw
+apt install ufw -y
 ufw allow http
 ufw allow https
 ufw allow 10000
